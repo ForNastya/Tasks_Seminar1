10227230cd6404a22fc2cab4a3864fdf79088991
Anastasiya <moya_pochta_n.k@mail.ru>
2022-10-03 19:18:44 +0300

    Task 03

---
+++ Task_03/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти максимальное из трех чисел
+int a = 7;
+int b = 3;
+int c = 9;
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+
+Console.WriteLine ("max = ");
+Console.WriteLine (max);
