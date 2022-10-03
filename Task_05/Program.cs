c3fa88878799c1b318e1bbbdd8f2ec46128e9f2f
Anastasiya <moya_pochta_n.k@mail.ru>
2022-10-04 00:51:20 +0300

    Task 05

---
+++ Task_05/Program.cs
@@ -0,0 +1,23 @@
+﻿//Показать четные числа от 1 до N
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+if (number > 0)
+{
+    int N = 2;
+    while (N <= number)
+    {
+        Console.Write(N);
+        Console.Write(", ");
+        N = N + 2;
+    }
+}
+if (number < 0)
+{
+    int N = -2;
+    while (N >= number)
+    {
+        Console.Write(N);
+        Console.Write(", ");
+        N = N - 2;
+    }
+}
