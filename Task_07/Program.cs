f21b4b8455b40196ed3089de13f525cdb5a864d0
Anastasiya <moya_pochta_n.k@mail.ru>
2022-10-04 12:44:40 +0300

    Task_07

--- Task_07/Program.cs
+++ Task_07/Program.cs
@@ -1,5 +1,3 @@
 ﻿// Показать вторую цифру трёхзначного числа
-Console.WriteLine();
-int number = new Random().Next(100, 1000);
-Console.WriteLine($"Второй цифрой числа {number} является цифра {number / 10 % 10}");
-Console.WriteLine();
+Console.WriteLine("Введите трехзначное число");
+Console.WriteLine("Вторая цифра = " + int.Parse(Console.ReadLine()) / 10 % 10);
