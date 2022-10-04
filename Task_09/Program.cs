08908483a6187f937ff13f82bf771194ca85c576
Anastasiya <moya_pochta_n.k@mail.ru>
2022-10-04 11:52:58 +0300

    Task 09

--- Task_09/Program.cs
+++ Task_09/Program.cs
@@ -1,11 +1,1 @@
 ﻿// Удалить вторую цифру трёхзначного числа
-int number = new Random().Next(100, 1000);
-int newNumber = 0;
-int numberDelete()
-{
-    newNumber = number / 100 * 10 + number % 10;
-    return newNumber;
-}
-numberDelete();
-Console.WriteLine();
-Console.WriteLine($"Если из числа {number} удалить вторую цифру, получится число {newNumber}");
