e817818e4e1eeee64a6ecc0488b3e68501ccad1a
Anastasiya <moya_pochta_n.k@mail.ru>
2022-10-04 09:44:34 +0300

    Task 06

---
+++ Task_06/Program.cs
@@ -0,0 +1,5 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.WriteLine("Введите трехзначное число");
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Последней цифрой числа {number} является цифра {number%10}");
+Console.WriteLine();
