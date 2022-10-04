5a68e1966496ac406d49516a3b90fed1fb022aa0
Anastasiya <moya_pochta_n.k@mail.ru>
2022-10-04 23:15:00 +0300

    Task 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти третью цифру числа с конца или сообщить, что ее нет
+int number = 123;
+if (number > 99)
+{
+    Console.WriteLine((number / 100)  % 10);
+}    
+else
+{
+  Console.WriteLine("Такого числа нет");  
+}
+
